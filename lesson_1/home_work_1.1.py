ab0dfcc8c3a9d34f1dfa4cdac6bc7e18d9111528
KirillZotov <zyzywin@mail.ru>
2023-02-07 12:18:21 +0300

    1.3

--- lesson_1/home_work_1.1.py
+++ lesson_1/home_work_1.1.py
@@ -1,7 +1,7 @@
 # Написать программу,определяющую,что
 # число трёхзначное и средняя цифра равна 5.
 
-numbers = int(input())
+numbers = 457 #int(input())
 
 is_three_digits = 0 < numbers // 100 <= 9
 num = numbers % 100 // 10
